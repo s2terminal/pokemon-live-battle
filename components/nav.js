f832c75f57e828e23a171a4978e64f6aba7cf955
s2terminal <suzuki.sh@s2terminal.com>
2020-02-14 02:01:52 +0900

    live battle room number

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+  { href: 'https://github.com/s2terminal/pokemon-live-battle', label: 'GitHubでソースをみる' },
 ].map(link => ({
   ...link,
   key: `nav-link-${link.href}-${link.label}`,
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,87 +2,127 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+/** 対戦1回をあらわす */
+class Battle {
+  readonly fighterA: string | undefined;
+  readonly fighterB: string | undefined;
+  readonly roomNumber: number;
 
-    <Nav />
+  constructor(fighterA: string | undefined, fighterB: string | undefined, roomNumber: number) {
+    this.fighterA = fighterA;
+    this.fighterB = fighterB;
+    this.roomNumber = roomNumber;
+  }
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+  public describe(): string {
+    const room = this.battleGroupNumber();
+    return `${this.fighterA}(対戦グループ番号: ${room}1) vs ${this.fighterB}(対戦グループ番号: ${room}2)`;
+  }
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+  public describeView(viewerNumber: number) {
+    const f = new Intl.NumberFormat('ja', { minimumIntegerDigits: 1 }).format;
+    return `観戦番号: ${this.battleGroupNumber()}${f(viewerNumber)}`;
+  }
+
+  public battleGroupNumber(): string {
+    const f = new Intl.NumberFormat('ja', { minimumIntegerDigits: 2 }).format;
+    return f(this.roomNumber);
+  }
+}
+
+/** 総当たり */
+function soatari(members: string[]): Battle[][] {
+  const ret = [];
+  // 5人なら5回(休1回)、6人なら5回、7人なら7回(休1回)...
+  for (let battleRound = 0; battleRound < Math.floor((members.length - 1) / 2) * 2 + 1; battleRound++) {
+    const battles = [];
+    const membersForRoop = [...members];
+    let left: string | null = null;
+    // 偶数人数だったら、末尾をパス
+    if (members.length % 2 === 0) {
+      left = membersForRoop.pop();
+    }
+
+    const dup = membersForRoop.slice(battleRound, membersForRoop.length).concat(membersForRoop.slice(0, battleRound));
+
+    // 以降、奇数確定
+    let battleNumber = 0;
+    // 5人なら2回、6人なら2回、7人なら3回...
+    for (; battleNumber < Math.floor((members.length - 1) / 2); battleNumber++) {
+      const battle = new Battle(dup.shift(), dup.pop(), battleNumber);
+      battles.push(battle);
+    }
+
+    // パスした人がいたら、残りの人と戦わせる
+    if (left !== null) {
+      battles.push(new Battle(left, dup.pop(), battleNumber));
+    }
+
+    ret.push(battles);
+  }
+  return ret;
+}
+
+// メインコンポーネント
+const Home = () => {
+  const title = 'ライブ大会の対戦ルーム番号';
+
+  const [membersString, setMembersString] = React.useState("タケシ, カスミ, マチス, エリカ");
+  const handleChange = (event) => {
+    setMembersString(event.target.value);
+  }
+  const battlesArray = soatari(membersString.split(","));
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav />
+
+      <div className="hero">
+        <h1 className="title">{title}</h1>
+        <p className="description">
+          対戦する人の名前をカンマ切りで入れてね
+        </p>
+        <input type="text" name="name" size={64} value={membersString} onChange={handleChange} />
+
+        <h2>総当たり対戦</h2>
+        {battlesArray.map((battles: Battle[], battleRound) => {
+          return (
+            <div key={battleRound}>
+              <h3>第{battleRound+1}回戦</h3>
+              {battles.map(battle => {
+                return <div key={battle.roomNumber}>{battle.describe()} {battle.describeView(3)}</div>;
+              })}
+            </div>);
+        })}
+
+        <h2>ライブ大会のやりかた</h2>
+        <p className="description">
+          <a href="https://www.pokemon.co.jp/ex/sword_shield/howtoplay/191108_vs.html">
+            『ポケモン ソード・シールド』で、バトル大会を楽しむ方法｜『ポケットモンスター ソード・シールド』公式サイト
+          </a>を読もう
+        </p>
+
       </div>
-    </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+      <style jsx>{`
+        .hero {
+          width: 100%;
+          color: #333;
+        }
+        .title {
+          margin: 0;
+          width: 100%;
+          line-height: 1.15;
+          font-size: 48px;
+        }
+      `}</style>
+    </div>
+  )
+};
 
 export default Home
